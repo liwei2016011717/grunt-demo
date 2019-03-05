7c64a5134f3be8c7209316e44d17ec13f50aacb4
wangding <408542507@qq.com>
2019-03-05 16:03:19 +0800

    htmlhint

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+exports = function(grunt) {
+  grunt.initConfig({
+    htmlhint: {
+    html: {
+        options: {
+            htmlhintrc: '.htmlhintrc'
+        },
+        src: ['*.html']
+          
+      }
+    }
+                
+  });
+
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+  grunt.registerTask('default', ['htmlhint']);
+
+};
