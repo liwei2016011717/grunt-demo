0123241d2fb3922b232cfe0808c137c41fc9a34d
wangding <408542507@qq.com>
2019-03-06 14:57:19 +0800

    eslint

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+exports = function(grunt) {
+    grunt.initConfig({
+    eslint: {
+      options: {
+           configFile: '.eslintrc.json'
+          
+      },
+        target: ['rectangle.js']
+        
+    }
+              
+  });
+
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.registerTask('default', ['eslint']);
+
+};
+
+
